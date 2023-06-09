bf8151de1eb96e4c388081bd11f491af6f8a206d
Ana Solcan <solcan.anna@gmail.com>
2023-06-09 21:43:01 +0200

    added static search bar

--- futurista/src/SearchBar.js
+++ futurista/src/SearchBar.js
@@ -1,9 +1,21 @@
+import React from "react";
+
 function SearchBar() {
-    return (
-      <div>
-        <h1>SearchBar</h1>
-      </div>
-    );
-  }
-  
-  export default SearchBar;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted");
+  };
+
+  return (
+    <div>
+      <h1>SearchBar</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter your job keyword here" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
