90172e4084a754569defcfbffc19355858f74391
norazics <nora.zics@yahoo.co.uk>
2023-06-10 08:47:37 +0100

    search bar

--- futurista/src/SearchBar.js
+++ futurista/src/SearchBar.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-function SearchBar() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission logic here
-    console.log("Form submitted");
-  };
+function SearchBar(props) {
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   // Handle form submission logic here
+  //   console.log("Form submitted");
+  // };
 
   return (
     <div>
       <h1>SearchBar</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Enter your job keyword here" />
+      <form >
+        <input type="text" placeholder="Enter your job keyword here" onChange={props.handleSearchBar}/>
         <button type="submit">Search</button>
       </form>
     </div>
