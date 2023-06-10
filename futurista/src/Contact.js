330ebc9b182d26685dbbc1f042f9a56874fd3cf8
norazics <nora.zics@yahoo.co.uk>
2023-06-10 16:34:01 +0100

    come on please deploy

--- futurista/src/Contact.js
+++ futurista/src/Contact.js
@@ -2,6 +2,26 @@ function Contact() {
     return (
       <div>
         <h1>Contact</h1>
+
+      <h1>Noni</h1>
+      <a
+        href="https://www.linkedin.com/in/nonisaurus/"
+        target="_blank"
+      >LinkedIn</a>
+      <a
+        href="https://github.com/nonisaurus/"
+        target="_blank"
+      >GitHub</a>
+
+      <h1>Ana</h1>
+      <a
+        href="https://www.linkedin.com/in/asolcan/"
+        target="_blank"
+      >LinkedIn</a>
+      <a
+        href="https://github.com/solcana/"
+        target="_blank"
+      >GitHub</a>
       </div>
     );
   }
